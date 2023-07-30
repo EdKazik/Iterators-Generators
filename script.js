d53e462aef4885074cd543ea6dbbb9835954c981
EdKazik <edoskazik@gmail.com>
2023-07-31 00:09:33 +0200

    merge with task branch

--- script.js
+++ script.js
@@ -25,9 +25,11 @@
 function Boo() {
   let i = 0;
   const arr = [1, 2];
+  const valuesArr = Object.values(arr);
 
+  console.log(valuesArr)
+  
   console.log('start');
-
     return {
       next: () => {
         if (i <= arr.length) {
